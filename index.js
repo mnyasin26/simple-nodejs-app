a74d86b8e8bd51c2029369cbc42db8d7e6db81f0
Muhamad Nur Yasin Amadudin <forsvive22@gmail.com>
2024-08-25 11:09:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World! This is me!');
+    res.send('Hello World! This is me! Rev2');
 });
 
 const port = process.env.PORT || 3000;
